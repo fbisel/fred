c5c901796f969668a2118538eb324cb23a1912c5
fbisel <61030189+fbisel@users.noreply.github.com>
2020-02-13 16:59:27 -0500

    Create index.php

---
+++ index.php
@@ -0,0 +1,14 @@
+<html>
+<title> LA James Jamerson Motown Bass</title>
+<body>
+<h1>
+<center>
+<IMG SRC="JamesJamerson_Bass.jpg" ALT="omg"><br>
+<?php
+$eip = file_get_contents('http://169.254.169.254/latest/meta-data/public-ipv4');
+echo $eip;
+?>
+</center>
+</h1>
+</body>
+</html>
